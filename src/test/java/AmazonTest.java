f74df57fa2672a88b7b2ead39504ca2014b3790f
rania chouaf <rania.chouaf@gmail.com>
2023-04-03 15:28:09 +0200

    add Google test

--- src/test/java/AmazonTest.java
+++ src/test/java/AmazonTest.java
@@ -22,4 +22,10 @@ public class AmazonTest {
     public void testAmazon() {
         driver.get("https://www.amazon.fr");
     }
+
+
+    @Test
+    public void testGoogle() {
+        driver.get("https://www.google.com");
+    }
 }
