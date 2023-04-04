4bc618604b52a27a1004122b8aee50b0fc7ef5f1
rania chouaf <rania.chouaf@gmail.com>
2023-04-04 11:04:58 +0200

    add goToSearchResultPage method

--- src/main/java/pageobjects/HomePage.java
+++ src/main/java/pageobjects/HomePage.java
@@ -3,11 +3,21 @@ package pageobjects;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class HomePage {
 
     By searchBarLocator = By.cssSelector("#twotabsearchtextbox");
     By cookiesLocator = By.id("sp-cc-accept");
+
+    By gamesAndConsoleLocator = By.cssSelector("[data-menu-id='12']");
+
+    By allCategoriesMenuLocator = By.cssSelector("#nav-main .nav-left");
+
+    By gamesCategoryLocator = By.cssSelector("ul.hmenu.hmenu-visible.hmenu-translateX > li a[class='hmenu-item']");
+
+    By allGamesLocator = By.cssSelector(".hmenu-visible > li a");
+
     WebDriver driver;
 
     public HomePage(WebDriver driver) {
@@ -20,5 +30,19 @@ public class HomePage {
 
     public void closeCookiePopup(){
         driver.findElement(cookiesLocator).click();
+    }
+
+
+    public void goToSearchResultPage(){
+        driver.findElement(allCategoriesMenuLocator).click();
+        Utils.setExplicitWait(driver);
+        Utils.wait.until(ExpectedConditions.elementToBeClickable(gamesAndConsoleLocator)).click();
+        driver.findElement(gamesCategoryLocator).click();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        driver.findElements(allGamesLocator).get(1).click();
     }
 }
