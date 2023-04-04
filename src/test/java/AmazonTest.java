26291f48844e73993cf825e837a042eef8e075b1
rania chouaf <rania.chouaf@gmail.com>
2023-04-04 10:42:47 +0200

    add iphone 13 test

--- src/test/java/AmazonTest.java
+++ src/test/java/AmazonTest.java
@@ -1,11 +1,17 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import pageobjects.CartPage;
+import pageobjects.HomePage;
+import pageobjects.ProductPage;
+import pageobjects.SearchResultPage;
 
-public class Iphone13Test {
+public class AmazonTest {
     WebDriver driver;
+
 
     @BeforeMethod
     public void setup() {
@@ -19,18 +25,26 @@ public class Iphone13Test {
     }
 
     @Test
-    public void testAmazon() {
+    public void testiphone13() {
         driver.get("https://www.amazon.fr");
+        String keyword = "iPhone 13";
+        HomePage homePage = new HomePage(driver);
+        SearchResultPage searchResult = new SearchResultPage(driver);
+        ProductPage productPage = new ProductPage(driver);
+        CartPage cartPage = new CartPage(driver);
+
+        homePage.closeCookiePopup();
+        homePage.search(keyword);
+
+        searchResult.openFirstProductPage();
+
+        productPage.addToCart();
+        productPage.refuseInsurance();
+        productPage.openCart();
+
+        Assert.assertTrue(cartPage.proceedToCheckoutButtonVisibility(), "Payment button not visible");
+        Assert.assertTrue(cartPage.getProductTitle().toLowerCase().contains(keyword.toLowerCase()), "The product title doesn't contain " + keyword);
+
     }
 
-
-    @Test
-    public void testGoogle() {
-        driver.get("https://www.google.com");
-    }
-
-    @Test
-    public void testYoutube() {
-        driver.get("https://www.youtube.com");
-    }
 }
