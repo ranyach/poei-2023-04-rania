9426d5c4406a54ba77bacd15c69714580957db9c
rania chouaf <rania.chouaf@gmail.com>
2023-04-04 11:50:27 +0200

    add GamesAndConsolesPAge object model

--- src/main/java/pageobjects/GamesAndConsolesPage.java
+++ src/main/java/pageobjects/GamesAndConsolesPage.java
@@ -1,2 +1,20 @@
-package pageobjects;public class GamesAndConsolesPage {
+package pageobjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class GamesAndConsolesPage {
+    By bestSellersLocator = By.cssSelector("[cel_widget_id='handsfree-browse_OctopusBestSellerAsin'] li");
+
+    WebDriver driver;
+
+    public GamesAndConsolesPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void openBestSeller(int index) {
+        driver.findElements(bestSellersLocator).get(index).click();
+    }
 }
