6eeb16af85a2cd15a4a2191e032be4b68b5daba7
rania chouaf <rania.chouaf@gmail.com>
2023-04-04 10:24:16 +0200

    add explicitWait Utils class

---
+++ src/main/java/pageobjects/Utils.java
@@ -0,0 +1,15 @@
+package pageobjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class Utils {
+
+    public static WebDriverWait wait;
+
+    public static void setExplicitWait(WebDriver driver, long duration){
+        wait = new WebDriverWait(driver, Duration.ofSeconds(duration));
+    }
+}
