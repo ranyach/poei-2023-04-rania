8411ca2beec7731f525a7b6d3a6ea0a05ae200e3
rania chouaf <rania.chouaf@gmail.com>
2023-04-04 11:50:56 +0200

    add Zelda test

--- src/test/java/AmazonTest.java
+++ src/test/java/AmazonTest.java
@@ -1,13 +1,11 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import pageobjects.CartPage;
-import pageobjects.HomePage;
-import pageobjects.ProductPage;
-import pageobjects.SearchResultPage;
+import pageobjects.*;
 
 public class AmazonTest {
     WebDriver driver;
@@ -25,7 +23,7 @@ public class AmazonTest {
     }
 
     @Test
-    public void testiphone13() {
+    public void testIphone13() {
         driver.get("https://www.amazon.fr");
         String keyword = "iPhone 13";
         HomePage homePage = new HomePage(driver);
@@ -44,6 +42,29 @@ public class AmazonTest {
 
         Assert.assertTrue(cartPage.proceedToCheckoutButtonVisibility(), "Payment button not visible");
         Assert.assertTrue(cartPage.getProductTitle().toLowerCase().contains(keyword.toLowerCase()), "The product title doesn't contain " + keyword);
+
+    }
+
+
+    @Test
+    public void testZelda() {
+        driver.get("https://www.amazon.fr");
+
+        HomePage homePage = new HomePage(driver);
+        SearchResultPage searchResult = new SearchResultPage(driver);
+        ProductPage productPage = new ProductPage(driver);
+        CartPage cartPage = new CartPage(driver);
+        GamesAndConsolesPage gamesAndConsolesPage = new GamesAndConsolesPage(driver);
+
+        homePage.closeCookiePopup();
+
+        homePage.goToSearchResultPage();
+        gamesAndConsolesPage.openBestSeller(0);
+
+        Assert.assertEquals(productPage.getPrice(),"54,99", "The price is not 54,99");
+
+
+        Assert.assertEquals(productPage.getAvailableDate(), "Cet article paraîtra le 12 mai 2023.", "The msg : Cet article ... is not visible ");
 
     }
 
